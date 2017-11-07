cc5ab1355cdfe3a16d4a3a0d0272b63ca1940b01
Johnny Nguyen <jnguyen1236@gmail.com>
2017-11-06 22:45:06 -0500

    Fixed up routes, and added leaf templating for html

---
+++ Sources/App/Controllers/HomeController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeController.swift
+//  App
+//
+//  Created by Johnny Nguyen on 2017-11-06.
+//
+
+import Vapor
+import HTTP
+
+public final class HomeController {
+	public func addRoutes(_ builder: RouteBuilder) {
+		
+	}
+}
--- Sources/App/Setup/Droplet+Setup.swift
+++ Sources/App/Setup/Droplet+Setup.swift
@@ -2,9 +2,13 @@
 
 extension Droplet {
 	public func setup() throws {
-		// add our collection
-		try self.collection(V1Collection.self)
-	
-		// later on, we'll probably need to set our routes to connect for the website
+		// MARK: Home Route
+		let homeController = HomeController()
+		
+		
+		// MARK: User Routes
+		let users = UserController()
+		let userRoutes = grouped("users")
+		users.addRoutes(userRoutes)
 	}
 }
