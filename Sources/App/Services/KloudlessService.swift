41f837c4d9bd056159b3c116d0c46f4df02d273d
Johnny Nguyen <jnguyen1236@gmail.com>
2017-12-19 11:50:28 -0500

    Setup basic service class

--- Sources/App/Services/KloudlessService.swift
+++ Sources/App/Services/KloudlessService.swift
@@ -10,5 +10,25 @@ import Foundation
  This class is a service request to the file API
 **/
 public class KloudlessService {
+  //MARK: Properties
+  private let apiKey: String
+  private let appId: String
   
+  /**
+   Sets up the API keys from initializing
+  **/
+  public init(appId: String, apiKey: String) {
+    self.appId = appId
+    self.apiKey = apiKey
+  }
+  
+  /**
+   A conveinence initalizer used to set up through the `Config` file. This is from the secrets folder
+  **/
+  public convenience init(config: Config) throws {
+    self.init(
+      appId: try config.get("appId"),
+      apiKey: try config.get("apiKey")
+    )
+  }
 }
