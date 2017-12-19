ae75b9f51a77bdfb8fff5423b2fc2c99dc360406
Johnny Nguyen <jnguyen1236@gmail.com>
2017-12-19 11:47:22 -0500

    Added kloudlesservice request api

---
+++ Sources/App/Services/KloudlessService.swift
@@ -0,0 +1,14 @@
+// KloudlessService.swift
+// Created by: Johnny Nguyen
+// Dec. 19 2017
+
+import Vapor
+import HTTP
+import Foundation
+
+/**
+ This class is a service request to the file API
+**/
+public class KloudlessService {
+  
+}
