37d8c97f3d1cc73f1d6584c305a3d2761931969e
Johnny Nguyen <jnguyen1236@gmail.com>
2017-12-19 21:17:14 -0500

    Setup basic for sessions and jwt

--- Sources/App/Middleware/AuthenticateMiddleware.swift
+++ Sources/App/Middleware/AuthenticateMiddleware.swift
@@ -4,7 +4,13 @@
 
 import Vapor
 import HTTP
+import Sessions
+import JWTProvider
 
+/**
+ This authentication middleware is used for protecting specific routes that we need a user to login.
+ Sometimes we may need the user to be an admin in order for us to get through
+**/
 public final class AuthenticateMiddleware: Middleware {
   public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
     return try next.respond(to: request)
